bd77c70277fa310665023946fa2cc5edbd2b51eb
Anmol Verma <anmol.verma4@gmail.com>
2022-05-22 21:19:23 +0530

    updated: harvest Themes.kt

--- webApp/src/jsMain/kotlin/common/Themes.kt
+++ webApp/src/jsMain/kotlin/common/Themes.kt
@@ -3,18 +3,36 @@ package common
 import kotlinx.js.jso
 import mui.material.PaletteMode.dark
 import mui.material.PaletteMode.light
+import mui.material.styles.ThemeOptions
 import mui.material.styles.createTheme
 
 object Themes {
     val Light = createTheme(
         jso {
-            palette = jso { mode = light }
+            palette = jso {
+                mode = light
+                primary = jso {
+                    main = "#fa5d00"
+                }
+                secondary = jso{
+                    main = "#f50057"
+                }
+            }
+
         }
     )
 
     val Dark = createTheme(
         jso {
-            palette = jso { mode = dark }
+            palette = jso {
+                mode = dark
+                primary = jso {
+                    main = "#fa5d00"
+                }
+                secondary = jso{
+                    main = "#f50057"
+                }
+            }
         }
     )
 }
