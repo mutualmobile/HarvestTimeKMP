3e8207f8a17542102621198e2630c3e958040634
Anmol Verma <anmol.verma4@gmail.com>
2022-05-24 22:20:55 +0530

    firebase hosting added!

--- webApp/webpack.config.d/fix.js
+++ webApp/webpack.config.d/fix.js
@@ -1,9 +1,9 @@
-config.devServer = {
+/*config.devServer = {
     open: true,
     port: 3000,
     static: [
         "/Users/anmolverma/projects/HarvestKMP/webApp/build/processedResources/js/main"
     ],
     historyApiFallback: true
-}
+}*/
 // TODO provide a way to set historyApiFallback to devServer
