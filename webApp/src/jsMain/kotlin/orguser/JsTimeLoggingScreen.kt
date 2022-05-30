da1b61bf6173a32b9156489c8fe06a35c0e8f523
Anmol Verma <anmol.verma4@gmail.com>
2022-05-30 17:16:43 +0530

    add: week view added

--- webApp/src/jsMain/kotlin/orguser/JsTimeLoggingScreen.kt
+++ webApp/src/jsMain/kotlin/orguser/JsTimeLoggingScreen.kt
@@ -2,9 +2,12 @@ package orguser
 
 import csstype.*
 import harvest.material.TopAppBar
+import kotlinx.css.whiteAlpha
 import kotlinx.datetime.internal.JSJoda.Clock
+import kotlinx.datetime.internal.JSJoda.LocalDate
 import kotlinx.datetime.internal.JSJoda.LocalDateTime
 import mui.material.Box
+import mui.material.Container
 import mui.material.Typography
 import mui.system.sx
 import react.FC
@@ -17,16 +20,16 @@ import kotlin.js.Date
 
 val JsTimeLoggingScreen = FC<Props> {
     var message by useState("")
-    val today = LocalDateTime.now(Clock.systemDefaultZone())
+    val today = LocalDate.now(Clock.systemDefaultZone())
     var selectedDate by useState(today)
     val navigate = useNavigate()
-    var week by useState(mutableListOf<LocalDateTime>())
+    var week by useState(mutableListOf<LocalDate>())
     val days = mutableListOf("Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun")
 
     fun generateWeek() {
-        val localWeek = mutableListOf<LocalDateTime>()
+        val localWeek = mutableListOf<LocalDate>()
         for (i in 1..7) {
-            val first =  today.minusDays(today.dayOfWeek().ordinal()).minusDays(1).plusDays(i)
+            val first = today.minusDays(today.dayOfWeek().ordinal()).minusDays(1).plusDays(i)
             localWeek.add(first)
         }
         week = localWeek
@@ -37,23 +40,33 @@ val JsTimeLoggingScreen = FC<Props> {
     }
 
     Box {
-        TopAppBar {
-            title = "Time logging for ${Date()}"
-            subtitle = message
+        Typography {
+            +"Time logging for ${Date()}"
         }
 
-        mui.material.List {
+        Container {
             sx {
-                display = Display.flex
-                flexDirection = FlexDirection.row
-                padding = 0.px
+                background = NamedColor.white
+                padding = 20.px
+                borderRadius = 10.px
+                marginBottom = 10.px
             }
-            week.mapIndexed { index, date ->
-                DayOfWeekView {
-                    weekDate = date.dayOfMonth().toInt()
-                    isToday = date == today
-                    isSelected = date.isEqual(selectedDate)
-                    weekTitle = days[index]
+            mui.material.List {
+                sx {
+                    display = Display.flex
+                    flexDirection = FlexDirection.row
+                    padding = 0.px
+                }
+                week.mapIndexed { index, date ->
+                    DayOfWeekView {
+                        weekDate = date.dayOfMonth().toInt()
+                        isToday = date == today
+                        isSelected = date.isEqual(selectedDate)
+                        weekTitle = days[index]
+                        onSelected = {
+                            selectedDate = date
+                        }
+                    }
                 }
             }
         }
@@ -65,16 +78,23 @@ external interface DayOfWeekViewProps : Props {
     var isToday: Boolean
     var isSelected: Boolean
     var weekTitle: String
+    var onSelected: ()->Unit
 }
 
 
 val DayOfWeekView = FC<DayOfWeekViewProps> { props ->
     Box {
         component = ReactHTML.div
+        onClick = {
+            props.onSelected()
+        }
+
         sx {
             display = Display.flex
             flexDirection = FlexDirection.column
             alignItems = AlignItems.center
+            borderRadius = 25.px
+            margin = 12.px
         }
         Typography {
             +props.weekTitle
@@ -86,6 +106,16 @@ val DayOfWeekView = FC<DayOfWeekViewProps> { props ->
             +props.weekDate.toString()
             sx {
                 padding = Padding(4.px, 4.px)
+            }
+        }
+        if(props.isSelected){
+            Box{
+                sx {
+                    width = 10.px
+                    height = 10.px
+                    borderRadius = 25.px
+                    color = NamedColor.darkred
+                }
             }
         }
     }
