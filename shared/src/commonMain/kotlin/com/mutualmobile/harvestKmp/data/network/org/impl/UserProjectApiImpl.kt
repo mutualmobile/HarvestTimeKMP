091a81ee5cebf4b05cb6e77f251819c4a0cbb47d
Anmol Verma <anmol.verma4@gmail.com>
2022-06-02 12:54:59 +0530

    impl: AssignProjectsToUsersDataModel and resolve conflicts from master

--- shared/src/commonMain/kotlin/com/mutualmobile/harvestKmp/data/network/org/impl/UserProjectApiImpl.kt
+++ shared/src/commonMain/kotlin/com/mutualmobile/harvestKmp/data/network/org/impl/UserProjectApiImpl.kt
@@ -2,6 +2,7 @@ package com.mutualmobile.harvestKmp.data.network.org.impl
 
 import com.mutualmobile.harvestKmp.data.network.Constants
 import com.mutualmobile.harvestKmp.data.network.Endpoint
+import com.mutualmobile.harvestKmp.data.network.getSafeNetworkResponse
 import com.mutualmobile.harvestKmp.data.network.org.UserProjectApi
 import com.mutualmobile.harvestKmp.domain.model.request.SignUpData
 import com.mutualmobile.harvestKmp.domain.model.response.ApiResponse
@@ -14,31 +15,13 @@ import io.ktor.http.*
 class UserProjectApiImpl(private val httpClient: HttpClient) : UserProjectApi {
 
     override suspend fun assignProjectsToUsers(
-        projectId: String,
-        userId: List<String>
+        projectMap:HashMap<String,List<String>>
     ): NetworkResponse<ApiResponse<Unit>> {
-        return try {
-            val response =
-                httpClient.post("${Endpoint.SPRING_BOOT_BASE_URL}${Endpoint.ASSIGN_PROJECT}") {
-                    contentType(ContentType.Application.Json)
-                    setBody(hashMapOf(Pair(projectId, userId)))
-//                    hashMapOf(Pair(projectId, userId))
-                }
-            when (response.status) {
-                HttpStatusCode.OK -> {
-                    NetworkResponse.Success(response.body())
-                }
-                HttpStatusCode.Unauthorized -> {
-                    NetworkResponse.Unauthorized(Throwable("Failed to authorize"))
-                }
-                else -> {
-                    val responseMsg = response.body<ApiResponse<Unit>>().message
-                    NetworkResponse.Failure(Exception(responseMsg))
-                }
+        return getSafeNetworkResponse {
+            httpClient.post("${Endpoint.SPRING_BOOT_BASE_URL}${Endpoint.ASSIGN_PROJECT}") {
+                contentType(ContentType.Application.Json)
+                setBody(projectMap)
             }
-        } catch (e: Exception) {
-            println(e)
-            NetworkResponse.Failure(e)
         }
     }
 
