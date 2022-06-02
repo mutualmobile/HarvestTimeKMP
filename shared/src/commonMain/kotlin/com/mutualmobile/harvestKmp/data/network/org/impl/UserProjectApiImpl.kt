b88a442cc504106fef183281b3516a4b33d04c5c
Anmol Verma <anmol.verma4@gmail.com>
2022-06-02 13:08:08 +0530

    update: AssignProjectsToUsersUseCase implemented with UI, TODO display a message when successfully assigned

--- shared/src/commonMain/kotlin/com/mutualmobile/harvestKmp/data/network/org/impl/UserProjectApiImpl.kt
+++ shared/src/commonMain/kotlin/com/mutualmobile/harvestKmp/data/network/org/impl/UserProjectApiImpl.kt
@@ -1,14 +1,11 @@
 package com.mutualmobile.harvestKmp.data.network.org.impl
 
-import com.mutualmobile.harvestKmp.data.network.Constants
 import com.mutualmobile.harvestKmp.data.network.Endpoint
 import com.mutualmobile.harvestKmp.data.network.getSafeNetworkResponse
 import com.mutualmobile.harvestKmp.data.network.org.UserProjectApi
-import com.mutualmobile.harvestKmp.domain.model.request.SignUpData
 import com.mutualmobile.harvestKmp.domain.model.response.ApiResponse
 import com.mutualmobile.harvestKmp.features.NetworkResponse
 import io.ktor.client.*
-import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 
