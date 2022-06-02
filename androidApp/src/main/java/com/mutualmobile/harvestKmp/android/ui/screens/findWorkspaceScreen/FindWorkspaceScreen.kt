b5295eb53cbd7d4f8e4a32c8bd444eca21af2a6b
Shubham Singh <shubham.singh@mutualmobile.com>
2022-06-02 20:45:29 +0530

    Created FindWorkspaceScreen UI

---
+++ androidApp/src/main/java/com/mutualmobile/harvestKmp/android/ui/screens/findWorkspaceScreen/FindWorkspaceScreen.kt
@@ -0,0 +1,136 @@
+package com.mutualmobile.harvestKmp.android.ui.screens.findWorkspaceScreen
+
+import android.content.res.Configuration
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.with
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mutualmobile.harvestKmp.MR
+import com.mutualmobile.harvestKmp.android.ui.theme.DrawerBgColor
+import com.mutualmobile.harvestKmp.android.ui.theme.FindWorkspaceScreenTypography
+import com.mutualmobile.harvestKmp.android.ui.theme.HarvestKmpTheme
+import com.mutualmobile.harvestKmp.android.ui.utils.get
+
+@Composable
+fun FindWorkspaceScreen() {
+    CompositionLocalProvider(LocalContentColor provides Color.White) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(DrawerBgColor)
+                .padding(16.dp)
+                .navigationBarsPadding()
+                .imePadding(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(verticalArrangement = Arrangement.Center) {
+                Text(
+                    text = MR.strings.find_workspace_screen_title.get(),
+                    style = FindWorkspaceScreenTypography.caption
+                )
+                CompositionLocalProvider(LocalTextStyle provides FindWorkspaceScreenTypography.h6) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp)
+                            .horizontalScroll(rememberScrollState())
+                    ) {
+                        Text(text = "https://")
+                        WorkspaceTextField()
+                        Text(text = ".harvestclone.com")
+                    }
+                }
+                Text(
+                    text = MR.strings.find_workspace_screen_subtitle.get(),
+                    style = FindWorkspaceScreenTypography.subtitle1
+                )
+            }
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = LocalContentColor.current
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+            ) {
+                Text(
+                    text = MR.strings.find_workspace_screen_btn_txt.get(),
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalAnimationApi::class)
+private fun WorkspaceTextField() {
+    var tfValue by remember { mutableStateOf("") }
+    BasicTextField(
+        value = tfValue,
+        onValueChange = { updatedValue -> tfValue = updatedValue },
+        textStyle = FindWorkspaceScreenTypography.h6.copy(color = Color.White),
+        decorationBox = { nonEmptyTextComposable ->
+            AnimatedContent(
+                targetState = tfValue.isEmpty(),
+                transitionSpec = { fadeIn() with fadeOut() }
+            ) { tfValueIsBlank ->
+                if (tfValueIsBlank) {
+                    Text(
+                        text = MR.strings.find_workspace_screen_placeholder_txt.get(),
+                        color = Color.White
+                    )
+                } else {
+                    nonEmptyTextComposable()
+                }
+            }
+        },
+        cursorBrush = SolidColor(LocalContentColor.current),
+        modifier = Modifier.width(IntrinsicSize.Max)
+    )
+}
+
+@Preview(
+    device = "id:pixel_4", showSystemUi = false,
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun FindWorkspaceScreenPreview() {
+    HarvestKmpTheme {
+        FindWorkspaceScreen()
+    }
+}
--- androidApp/src/main/java/com/mutualmobile/harvestKmp/android/ui/utils/GeneralUtils.kt
+++ androidApp/src/main/java/com/mutualmobile/harvestKmp/android/ui/utils/GeneralUtils.kt
@@ -1,3 +1,10 @@
 package com.mutualmobile.harvestKmp.android.ui.utils
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import dev.icerock.moko.resources.StringResource
+
 fun Number.toDecimalString(decimalPlaces: Int = 2) = "%.${decimalPlaces}f".format(this)
+
+@Composable
+fun StringResource.get() = stringResource(id = resourceId)
