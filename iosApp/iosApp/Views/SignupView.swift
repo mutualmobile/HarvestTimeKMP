112db412931b43061b3740819c26eff7b579dc05
Nasir Ahmed Momin <nasir.momin@mutualmobile.com>
2022-06-03 20:22:38 +0530

    Basic Signup UI is constructed

--- iosApp/iosApp/Views/SignupView.swift
+++ iosApp/iosApp/Views/SignupView.swift
@@ -12,16 +12,98 @@ struct SignupView: View {
     
     @Environment(\.dismiss) var dismiss
     
+    @State private var fullname = ""
+    @State private var companyName = ""
+    @State private var companyEmail = ""
+    @State private var password = ""
+    
+    
+    init() {
+        UINavigationBar.appearance().backgroundColor = UIColor(ColorAssets.colorBackground.color)
+        UINavigationBar.appearance().tintColor = UIColor(ColorAssets.white.color)
+    }
+    
     var body: some View {
         NavigationView {
-
-            SwiftUI.Form {
-                Text("Hello, World!")
+            VStack {
+                googleSignInButton
+                LabelledDivider(label: "or with you email below")
+                credentialView
+                signunButton
             }
             
             .navigationTitle("Sign Up")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Cancel")
+                            .foregroundColor(ColorAssets.white.color)
+                    }
+                }
+            }
         }
-        
+    }
+    
+    private var googleSignInButton: some View {
+        Button {
+            // TODO: (Nasir) Need to handle
+        } label: {
+            // TODO: (Nasir) Need to remove this entire HStack for Google Sign In, Must use button provided by Google pod
+            HStack {
+                Image("Google-Icon").padding(.trailing)
+                Text("Sign In with Google").padding(.leading)
+            }
+            .harvestButton()
+        }
+        .padding(.bottom)
+    }
+    
+    private var credentialView: some View {
+        VStack {
+            fullNameView
+            companyNameView
+            companyEmailView
+            passwordView
+        }.padding(.horizontal)
+    }
+    
+    private var fullNameView: some View {
+        HStack {
+            Text("Full Name :")
+            TextField("Tony Stark", text: $fullname)
+        }.padding(.vertical)
+    }
+    
+    private var companyNameView: some View {
+        HStack {
+            Text("Company :")
+            TextField("Stark Tech", text: $companyName)
+        }.padding(.bottom)
+    }
+    
+    private var companyEmailView: some View {
+        HStack {
+            Text("Email:")
+            TextField("tony.stark@yyo.com", text: $companyEmail)
+        }.padding(.bottom)
+    }
+    
+    private var passwordView: some View {
+        HStack {
+            Text("Password: ")
+            SecureField("tony.stark@yyo.com", text: $password)
+        }.padding(.bottom)
+    }
+    
+    private var signunButton: some View {
+        Button {
+            // TODO: Nasir, Handle action
+        } label: {
+            Text("SIGN UP")
+        }
+        .harvestButton()
     }
 }
 
