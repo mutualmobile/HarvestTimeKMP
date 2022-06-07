5e8dbbbf876fb670bdd771de9d76613b0be55c8e
Pushpal Roy <pushpal.roy@mutualmobile.com>
2022-06-08 01:22:33 +0530

    Upgrade jetbrains compose gradle plugin to 1.2.0-alpha01-dev686

--- compose-desktop/src/jvmMain/kotlin/Main.kt
+++ compose-desktop/src/jvmMain/kotlin/Main.kt
@@ -1,6 +1,12 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.mutualmobile.harvestKmp.di.initSharedDependencies
@@ -8,7 +14,16 @@ import com.mutualmobile.harvestKmp.di.initSharedDependencies
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {}
+    MaterialTheme {
+      Column {
+        Text(
+          text = "Hello KMM Desktop!",
+          modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+        )
+      }
+    }
 }
 
 fun main() = application {
